9c39173e93d4fe9df59960bb78a9691e2906dffc
Marlon Raphael <raphael27atm@gmail.com>
2019-03-17 10:06:49 -0300

    Add AsyncStorage

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import UdaciSteppers from './UdaciSteppers'
 import DateHeader from './DateHeader'
 import { Ionicons } from '@expo/vector-icons'
 import TextButton from './TextButton'
+import { submitEntry, removeEntry } from '../utils/api'
 
 function SubmitBtn ({ onPress }) {
   return (
@@ -62,6 +63,7 @@ export default class AddEntry extends Component {
     // Navigate to home
 
     // Save to "DB"
+    submitEntry({ key, entry })
 
     // Clear local notification
   }
@@ -73,6 +75,7 @@ export default class AddEntry extends Component {
     // Route to Home
 
     // Update "DB"
+    removeEntry(key)
   }
   render() {
     const metaInfo = getMetricMetaInfo()
